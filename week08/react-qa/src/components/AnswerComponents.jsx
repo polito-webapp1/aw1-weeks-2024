0208da780900397d836190cf59bdb88f988563ad
Luigi De Russis <luigidr@gmail.com>
2024-04-30 11:45:23 +0200

    add exercise done in class

--- week08/react-qa/src/components/AnswerComponents.jsx
+++ week08/react-qa/src/components/AnswerComponents.jsx
@@ -1,16 +1,19 @@
 /* eslint-disable react/prop-types */
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import { Row, Col, Table, Button } from "react-bootstrap";
+import FormAnswer from './FormAnswer';
 
 function Answers (props) {
+
   return(
     <>
     <Row>
-      <Col as='h2'>Answers:</Col>
+      <Col as='h2'>Answers ({props.answers.length}):</Col>
     </Row>
     <Row>
       <Col lg={10} className="mx-auto">
-        <AnswerTable answers={props.answers}></AnswerTable>
+        <AnswerTable answers={props.answers} voteUp={props.voteUp}></AnswerTable>
+        <FormAnswer />
       </Col>
     </Row>
     </>
@@ -30,7 +33,7 @@ function AnswerTable (props) {
         </tr>
       </thead>
       <tbody>
-        { props.answers.map((ans) => <AnswerRow answer={ans} key={ans.id}/>) }
+        { props.answers.map((ans) => <AnswerRow answer={ans} key={ans.id} voteUp={props.voteUp}/>) }
       </tbody>
     </Table>
   );
@@ -38,7 +41,7 @@ function AnswerTable (props) {
 
 function AnswerRow(props) {
   return(
-    <tr><AnswerData answer={props.answer}/><AnswerAction /></tr>
+    <tr><AnswerData answer={props.answer}/><AnswerAction answerId={props.answer.id} voteUp={props.voteUp}/></tr>
   );
 }
 
@@ -53,10 +56,12 @@ function AnswerData(props) {
   );
 }
 
-function AnswerAction() {
+function AnswerAction(props) {
   return(
     <td>
-      <Button variant='warning'><i className='bi bi-arrow-up'></i></Button>
+      <Button variant='warning' onClick={
+        () => props.voteUp(props.answerId)
+      }><i className='bi bi-arrow-up'></i></Button>
       <Button variant='primary' className='mx-1'><i className='bi bi-pencil-square'></i></Button> 
       <Button variant='danger'><i className='bi bi-trash'></i></Button>
     </td>
